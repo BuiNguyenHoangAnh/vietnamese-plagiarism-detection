7c68b83aa11f7367e3c734455f4f547127415785
BuiNguyenHoangAnh <hoanganhbui099@gmail.com>
2022-08-11 00:06:13 +0700

    update data for near & modified copy

--- test_data/README.txt
+++ test_data/README.txt
@@ -1,2 +1,4 @@
 pos: các trường hợp vi phạm đạo văn
 neg: các trường hợp không vi phạm đạo văn
+=====
+rephrase.info: tạo các trường hợp near + modified copy
--- test_data/extract_row_excel_to_txt.py
+++ test_data/extract_row_excel_to_txt.py
@@ -1,7 +1,7 @@
 
 import pandas as pd 
 
-df=pd.read_csv("exact_copy.csv",sep=",")
+df=pd.read_csv("near_modified_copy.csv",sep=",")
 
 for index in range(len(df)):
      with open(df["no."][index] +  '.txt', 'w', encoding="utf-8") as output:
